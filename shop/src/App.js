210ca47bf7ead10cbb0e470f391a6caad75fed20
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2023-07-30 23:13:29 +0900

    rexux tookit 사용 및 localStorage 이용법

---
+++ shop/src/App.js
@@ -0,0 +1,120 @@
+import {Button,Container,Nav,Navbar,Row,Col} from 'react-bootstrap';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { data } from './data.js'
+import { Detail } from './pages/detail';
+import { Cart } from './pages/Cart'
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import axios from 'axios';
+
+function App() {
+
+  useEffect(()=>{
+    localStorage.setItem('watched', JSON.stringify([]))
+  },[])
+
+
+  let [shoes, setShoes] = useState(data)
+  let [click, setClick] = useState(0)
+  let [loading, setLoading] = useState(false)
+  let navigate = useNavigate()
+  
+
+
+  return (
+    <div className="App">
+
+    
+
+      <Navbar className='nav-bar' data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">상결신발</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=>{ navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/Cart')}}>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+     
+
+      <Routes>
+        <Route path="/" element={
+        <>
+          <div className='main-bg'></div>
+          <Container>
+          <Row>
+            {
+              shoes.map((a, i)=>{
+                return(
+                  <Card shoes={shoes[i]} i={i}/>
+                )
+              })
+            }
+          </Row>
+          </Container>
+          {
+            loading == true ? 
+            <div className="alert alert-warning">
+              로딩중입니다.
+            </div> : null
+          }
+          <button onClick={()=>{
+            if(click == 0){
+              setLoading(true)
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((result)=>{ 
+                let copy = [...shoes, ...result.data]
+                setShoes(copy)
+                setLoading(false)
+              })
+              .catch((err)=>{
+                console.log(err)
+              })
+              setClick(click+1)
+            }else if(click == 1){
+              setLoading(true)
+              axios.get('https://codingapple1.github.io/shop/data3.json')
+              .then((result)=>{ 
+                let copy = [...shoes, ...result.data]
+                setShoes(copy)
+                setLoading(false)
+              })
+              .catch((err)=>{
+                console.log(err)
+              })
+              setClick(click+1)
+            }else{
+              alert('데이터 더 없음')
+            }
+          }}>버튼</button>
+        </>
+      }/>
+      
+        <Route path="/detail/:id" element={ <Detail shoes={shoes}/> }/>
+
+        <Route path="/cart" element={ <Cart/> }/>
+
+        <Route path="*" element={ <div>없는페이지에요</div> }/>
+      </Routes>
+    </div>
+  );
+}
+
+function Card(props){
+
+  let navigate = useNavigate()
+  return(
+          <Col>
+          
+            <img src={"https://codingapple1.github.io/shop/shoes"+(props.shoes.id+1)+".jpg"} width="300px"  onClick={()=>{ navigate('/detail/'+props.shoes.id)}}/>
+          <div style={{textAlign : 'left'}}>
+            <h4 >{props.shoes.title}</h4>
+            <p >{props.shoes.price}</p>
+          </div>
+          </Col>
+  )
+}
+
+export default App;
---
+++ shop/src/pages/detail.js
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav'
+import { addItem } from './../store' 
+import { useDispatch } from "react-redux";
+
+
+
+
+function Detail(props){
+    let [warn, setWarn] = useState(true)
+    let [alert, setAlert] = useState(false)
+    let [tab, setTab] = useState(0)
+    let [fade, setFade] = useState('')
+    let {id} = useParams();
+    let 찾은상품 = props.shoes.find((e)=>{return e.id == id})
+    let [content, setContent] = useState(0)
+
+    let dispatch = useDispatch()
+
+    useEffect(()=>{
+        setFade('end')
+        let a = setTimeout(()=>{ setWarn(false) },2000)
+        return()=>{ 
+            clearTimeout(a)
+        }//기존 타이머 제거
+    },[fade, warn])
+
+    useEffect(()=>{
+        let 꺼낸거 = localStorage.getItem('watched')
+        꺼낸거 = JSON.parse(꺼낸거)
+        꺼낸거.push(찾은상품.id)
+        꺼낸거 = [...new Set(꺼낸거)]
+        localStorage.setItem('watched', JSON.stringify(꺼낸거))
+    },[])
+        
+
+
+    
+
+
+
+    if(isNaN(id)==true){//id에 숫자가 아닌게 들어가면 true반환
+        return(
+            <div>숫자만입력하시오</div>
+        )
+    }else{
+        return(
+            <div className={"container start " + fade}>
+                {
+                    warn == true ? 
+                    <div className="alert alert-warning">
+                        2초내 구매시 할인
+                    </div> : null
+                }
+                <div className="row">
+                <div className="col-md-6">
+                    <img src={"https://codingapple1.github.io/shop/shoes"+(찾은상품.id+1)+".jpg"} width="100%" />
+                </div>
+                <div className="col-md-6">
+                    <h4 className="pt-5">{찾은상품.title}</h4>
+                    <p>{찾은상품.content}</p>
+                    <p>{찾은상품.price}</p>
+                    {
+                        alert == true ? 
+                        <div className="alert alert-warning">
+                            숫자만 쓰시오
+                        </div> : null
+                    }
+                    {/* <input onChange={(e)=>{
+                        setContent(e.target.value) 
+                        if(isNaN(content)){
+                            setAlert(true)
+                        }else{
+                            setAlert(false)
+                        }
+                    }}/><br/><br/> */}
+                    <button className="btn btn-danger" onClick={()=>{
+                        dispatch(addItem(찾은상품))
+                    }}>주문하기</button> 
+                    
+                </div>
+                </div>
+
+                <Nav variant="tabs"  defaultActiveKey="link0">
+                    <Nav.Item>
+                        <Nav.Link eventKey="link0" onClick={()=>{setTab(0)}}>버튼0</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link1" onClick={()=>{setTab(1)}}>버튼1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link2" onClick={()=>{setTab(2)}}>버튼2</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                {/* {tab == 0 ? <div>내용0</div> : null}
+                {tab == 1 ? <div>내용1</div> : null}
+                {tab == 2 ? <div>내용2</div> : null} */}
+                <TabContent tab={tab}/>
+                
+                
+                
+
+            </div> 
+        )
+    }
+    
+}
+
+function TabContent(props){
+
+    let [fade, setfade] = useState('')
+
+    useEffect(()=>{
+        setTimeout(()=>{setfade('end')}, 70)
+        
+        return ()=>{
+            setfade('')
+        }
+    },[props.tab])
+
+    return (<div className={'start ' + fade }>
+        { [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][props.tab]} 
+        </div>)
+}
+
+
+
+export {Detail}

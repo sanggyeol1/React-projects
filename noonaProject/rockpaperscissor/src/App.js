58c976acdbfb57d103b461701dab5b731ba27d26
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-03-20 18:35:24 +0900

    가위바위보 기능 구현 완료

--- noonaProject/rockpaperscissor/src/App.js
+++ noonaProject/rockpaperscissor/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import Box from './conponent/Box';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHand, faHandScissors, faHandBackFist } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -12,37 +14,68 @@ import Box from './conponent/Box';
 //6. 승패 결과에 따라 테두리 색이 바뀐다.
 
 const choice = {
-  rock : {
-    name : "Rock",
-    image : 'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi1.sndcdn.com%2Fartworks-Zx3I74NthX78Nk2t-UAG4bw-t500x500.jpg&type=a340'
+  rock: {
+    name: "Rock",
+    image: 'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi1.sndcdn.com%2Fartworks-Zx3I74NthX78Nk2t-UAG4bw-t500x500.jpg&type=a340'
   },
   scissors: {
-    name : "Scissors",
-    image : "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fsitem.ssgcdn.com%2F10%2F67%2F84%2Fitem%2F1000548846710_i4_500.jpg&type=a340"
+    name: "Scissors",
+    image: "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fsitem.ssgcdn.com%2F10%2F67%2F84%2Fitem%2F1000548846710_i4_500.jpg&type=a340"
   },
-  paper : {
-    name : "Paper",
-    image : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDAxMjRfODMg%2FMDAxNzA2MDc5MjE2NDYw.HXKOeo_7Fu0N93EA7zL3OFFi5ciRI6L1-itnpu6ciAQg.y1gSCUmRZOekdtl_AJQ4Qq0hyGA-KX3gE_WpBelvcQkg.JPEG.gopaper_online%2F%25B9%25AB%25B4%25CC%25C6%25DE%25C1%25F6_%25BD%25E6%25B3%25D7%25C0%25CF.jpg&type=sc960_832"
+  paper: {
+    name: "Paper",
+    image: "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDAxMjRfODMg%2FMDAxNzA2MDc5MjE2NDYw.HXKOeo_7Fu0N93EA7zL3OFFi5ciRI6L1-itnpu6ciAQg.y1gSCUmRZOekdtl_AJQ4Qq0hyGA-KX3gE_WpBelvcQkg.JPEG.gopaper_online%2F%25B9%25AB%25B4%25CC%25C6%25DE%25C1%25F6_%25BD%25E6%25B3%25D7%25C0%25CF.jpg&type=sc960_832"
   }
 }
 
 
 
 function App() {
-  const [userSelect, setUserSelect] = useState() 
-  const play = (userChoice)=>{
+  const [userSelect, setUserSelect] = useState(null)
+  const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState("")
+  const [computerResult, setComputerResult] = useState("")
+
+  const play = (userChoice) => {
     setUserSelect(choice[userChoice])
+    let computerChoice = randomChioce()
+    setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice], computerChoice))
+    setComputerResult(judgement(computerChoice, choice[userChoice]))
   }
+
+  const judgement = (user, computer) => {
+
+    console.log(user, computer)
+    if (user.name == computer.name) {
+      return "Tie"
+    } else if ((user.name == "Rock" && computer.name == "Scissors") ||
+      (user.name == "Scissors" && computer.name == "Paper") ||
+      (user.name == "Paper" && computer.name == "Rock")) {
+      return "Win"
+    }
+    else {
+      return "Lose"
+    }
+  }
+
+  const randomChioce = () => {
+    let itemArray = Object.keys(choice)//객체의 키값만 뽑아서 array로 변환
+    let randomItem = Math.floor(Math.random() * itemArray.length)
+    let final = itemArray[randomItem]
+    return choice[final]
+  }
+
   return (
     <>
       <div className='main'>
-        <Box title="You" item={userSelect}/>
-        <Box title="Computer" item={userSelect}/>
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={computerResult} />
       </div>
       <div className='main'>
-        <button onClick={ ()=>{play("scissors")}}>가위</button>
-        <button onClick={ ()=>{play("rock")}}>바위</button>
-        <button onClick={ ()=>{play("paper")}}>보</button>
+        <FontAwesomeIcon className='icon' icon={faHandScissors} onClick={() => { play("scissors") }} />
+        <FontAwesomeIcon className='icon' icon={faHandBackFist} onClick={() => { play("rock") }} />
+        <FontAwesomeIcon className='icon' icon={faHand} onClick={() => { play("paper") }} />
       </div>
     </>
   );
--- noonaProject/rockpaperscissor/src/conponent/Box.js
+++ noonaProject/rockpaperscissor/src/conponent/Box.js
@@ -2,12 +2,23 @@ import React from 'react'
 
 const Box = (props) => {
 
-    console.log(props)
+  const borderColor = getBorderColor(props.result)
+  const boxStyle = {
+    border: '5px solid ' + borderColor
+  }
+
+
+  function getBorderColor(result) {
+    if(result == 'Win') return 'green'
+    else if(result == 'Lose') return 'red'
+    else return 'black'
+  }
+
   return (
-    <div className='box'>
-        <h1>{props.title}</h1>
-        <img className='item-img' src={props.item && props.item.image}/>
-        <h2>WIN</h2>
+    <div className='box' style={boxStyle}>
+      <h1>{props.title}</h1>
+      <img className='item-img' src={props.item && props.item.image} />
+      <h2>{props.result}</h2>
     </div>
   )
 }

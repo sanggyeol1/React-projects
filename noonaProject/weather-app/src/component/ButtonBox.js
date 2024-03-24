bcd14437fac3bd813af869aa44bfcf0a42ce9b23
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-03-25 01:58:54 +0900

    날씨 앱, loading spinner

--- noonaProject/weather-app/src/component/ButtonBox.js
+++ noonaProject/weather-app/src/component/ButtonBox.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
+import { Button } from 'react-bootstrap';
 
 
 const ButtonBox = (props) => {
   return (
     <>
+      <div className='button-box'>
+        <Button variant="warning" onClick={() => { props.getCurrentLocation() }}>
+          현재위치
+        </Button>
+
+        
+        {
+          props.cities.map((item, index) => {
+            return (
+              <Button variant="warning" key={index} onClick={() => { props.setCity(item) }}>
+                {item}
+              </Button>
+            )
+          })
+        }
+      </div>
     </>
   )
 }

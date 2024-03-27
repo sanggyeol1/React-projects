231cd39e619d1dcaf4de7ca33e5ceca8376dfaaa
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-03-28 01:36:44 +0900

    db.json

--- noonaProject/hnm-react-router-practice/src/page/MainPage.js
+++ noonaProject/hnm-react-router-practice/src/page/MainPage.js
@@ -8,7 +8,7 @@ const MainPage = () => {
   const [productList, setProductList] = useState([])
 
   const getProducts = async () => {
-    let url = 'http://localhost:5000/products'
+    let url = 'https://my-json-server.typicode.com/sanggyeol1/React.js/products'
     let response = await fetch(url)
     let data = await response.json()
     setProductList(data)

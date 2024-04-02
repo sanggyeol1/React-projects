ba6dbd6c59bd17827dcb7596123dbac7885fabb6
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-04-03 00:54:07 +0900

    redux 미들웨어, redux devtools

---
+++ noonaProject/hnm-react-router-practice/src/redux/actions/authenticateAction.js
@@ -0,0 +1,8 @@
+function login(id, password) {
+    return (dispatch, getState) => {
+        //async 함수 하고 나서
+        dispatch({ type: "LOGIN_SUCCESS", payload: { id, password } })//디스패치 부름
+    }
+}
+
+export const authenticateAction = { login }
---
+++ noonaProject/hnm-react-router-practice/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import authenticateReducer from "./authenticateReducer";
+import productReducer from './productReducer'
+
+
+//행동지침 리듀서를 여러 개 만들면 컴바인리듀서로 합쳐줘야 한다.
+export default combineReducers({
+    auth: authenticateReducer,
+    product: productReducer,
+})

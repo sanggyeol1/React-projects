1328b4bb9cd49f237c245cbbe3eba36c9849c9dd
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-04-08 23:29:14 +0900

    Revert "navbar 수정"
    This reverts commit deabf7a3e354fd0be721f322001f001dc8d0d7ec.

--- noonaProject/netflix-demo/src/layout/AppLayout.jsx
+++ noonaProject/netflix-demo/src/layout/AppLayout.jsx
@@ -5,19 +5,16 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Outlet, Link } from 'react-router-dom'
-import '../App.css'
 
 const AppLayout = () => {
     return (
         <div>
-            <Navbar expand="lg" className="bg-black ">
+            <Navbar className="bg-black">
                 <Container fluid className='bg-black'>
                     <Navbar.Brand href="">
-                        <Link to="/">
-                            <img width={120} src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzExMjdfMTk5%2FMDAxNzAxMDY3MDQxNzgy.sOB9wfLjeOj1Ap4DmnvSZq0heha8wTkuQBb6GThE914g.ed-w_z0DqgcBF9aj-QPEBUG1kUEd5fu-G5wE7rUXvJ4g.PNG.qoxkf202%2F%25B3%25DD%25C7%25C3%25B8%25AF%25BD%25BA.png&type=l340_165" />
-                        </Link>
+                        <img width={120} src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzExMjdfMTk5%2FMDAxNzAxMDY3MDQxNzgy.sOB9wfLjeOj1Ap4DmnvSZq0heha8wTkuQBb6GThE914g.ed-w_z0DqgcBF9aj-QPEBUG1kUEd5fu-G5wE7rUXvJ4g.PNG.qoxkf202%2F%25B3%25DD%25C7%25C3%25B8%25AF%25BD%25BA.png&type=l340_165"/>
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
                             className="me-auto my-2 my-lg-0 cl-white"
@@ -28,13 +25,12 @@ const AppLayout = () => {
                             <Link to="/movies">Movies</Link>
                         </Nav>
                         <Form className="d-flex">
-                            <Form.Control
+                            <input
                                 type="search"
                                 placeholder="Search"
-                                className="me-2 custom-form-control"
-
+                                className="me-2 form-input-box"
                             />
-                            <Button variant="outline-danger">Search</Button>
+                            <Button variant="outline-danger">search</Button>
                         </Form>
                     </Navbar.Collapse>
                 </Container>
